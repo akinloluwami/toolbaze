akinloluwami/manytools

feat: Add color code converter

src/tools.tsx
@@ -79,4 +79,10 @@ export default [
     icon: SolarIconSet.RefreshCircle,
     category: "utility",
   },
+  {
+    title: "Color Code Converter",
+    route: "/color-code-converter",
+    icon: SolarIconSet.Hashtag,
+    category: "image"
+  }
 ] satisfies Tool[];

akinloluwami/toolbaze

src/tools.tsx
@@ -80,6 +80,12 @@ export default [
     category: "utility",
   },
   {
+    title: "Color Code Converter",
+    route: "/color-code-converter",
+    icon: SolarIconSet.RefreshCircle,
+    category: "image"
+  },
+  {
     title: "Password Generator",
     route: "/password-generator",
     icon: SolarIconSet.LockPassword,